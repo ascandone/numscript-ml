7bd65b7c8ff55965bc656a10771ef128335e7103
ascandone <alescandone@gmail.com>
2026-06-09 10:17:23 +0200

    exposed run

---
+++ lib/vm_register/run.ml
@@ -0,0 +1,9 @@
+let run_program ~vars ~balances (program : Syntax.Ast.program) =
+  let ( let* ) = Result.bind in
+  let* compiled =
+    Compiler.compile_parsed program |> Result.map_error (fun e -> `Compilation e)
+  in
+  let vm = Abstract_vm.create ~instructions:compiled.instructions in
+  let postings = Abstract_vm.run vm ~vars ~balances in
+  Ok postings
+;;
---
+++ lib/vm_register/run.mli
@@ -0,0 +1,7 @@
+val run_program
+  :  vars:string Common.Run_state.StringMap.t
+  -> balances:int64 Common.Run_state.PairsMap.t
+  -> Syntax.Ast.program
+  -> ( Common.posting list
+       , [> `Compilation of Compiler.compilation_err | `Runtime of Common.run_error ] )
+       result
